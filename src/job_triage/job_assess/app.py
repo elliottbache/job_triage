1c119acbffc87cb4370113782b8ee9b5f1be54be
Elliott Bache <elliott.bache@gmail.com>
2026-04-22 22:42:48 +0200

    feat: add function stubs for salary estimation and grading

---
+++ src/job_triage/job_assess/app.py
@@ -0,0 +1,4 @@
+# estimate_salary()
+# grade_required_stack()
+# compare_my_stack_to_theirs(): this gives individual_fit_scores: dict[str, Annotated[int, Field(ge=0, le=100)]] 
+# calculate_final_grade()
